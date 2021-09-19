84833c205c8608e19df20323e5386bf4a3af5bc0
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-09-19 17:51:29 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-  
+    <Counter></Counter>
     </div>
   );
 }
 
 function Counter(){
-
+  const [count, setCount] = useState(34);
+  const handleIncrese = () => setCount(count + 1);
+  const handleDecrese = () => setCount(count - 1);
+  
   return(
     <div>
-      <h2>Counter : </h2>
-      <button>Increse</button>
-      <button>Decrese</button>
+      <h2>Counter : {count} </h2>
+      <button onClick={handleIncrese}>Increse</button>
+      <button onClick={handleDecrese}>Decrese</button>
     </div>
   )
 }
