8f08626cc93653d19e4a7ac82dec005d003d18e7
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-09-19 17:27:59 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+  
+    </div>
+  );
+}
+
+function Counter(){
+
+  return(
+    <div>
+      <h2>Counter : </h2>
+      <button>Increse</button>
+      <button>Decrese</button>
+    </div>
+  )
+}
+export default App;
